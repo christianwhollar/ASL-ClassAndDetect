e551652d9f962dc067d081766f8e2e8cc2023b51
christianwhollar <christian.hollar@duke.edu>
2023-06-25 16:20:22 -0400

    svm model class, setup

--- scripts/svm_model.py
+++ scripts/svm_model.py
@@ -1,24 +1,106 @@
+import joblib 
 import json
-with open('classes.json', 'r') as f:
-        classes = json.load(f)
+import numpy as np
+import os
+import pandas as pd
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn import svm
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+class SVMModel():
+    '''
+    SVM Model Class for Image Classification
+    4 Parts: Load, Train, Test, Setup
+    Optimal params found using GridSearchCV
+    '''
+    def __init__(self):
+        with open('classes.json', 'r') as f:
+            self.classes = json.load(f)
+        self.datadir = os.getcwd() + '/data/processed/train'
         
-Categories=classes.keys()
-
-flat_data_arr=[] #input array
-target_arr=[] #output array
-
-datadir='IMAGES/' 
-
-#path which contains all the categories of images
-for i in Categories:
-    print(f'loading... category : {i}')
-    path=os.path.join(datadir,i)
-    for img in os.listdir(path):
-        img_array=imread(os.path.join(path,img))
-        img_resized=resize(img_array,(150,150,3))
-        flat_data_arr.append(img_resized.flatten())
-        target_arr.append(Categories.index(i))
-    print(f'loaded category:{i} successfully')
-    
-flat_data=np.array(flat_data_arr)
-target=np.array(target_arr)
+    def load(self):
+        '''
+        Load in data to Pandas DataFrame.
+        Split into test/train data.
+        Set processed data to self.
+        Args:
+            None
+        Returns:
+            None
+        '''
+        flat_data_arr = []
+        target_arr = []
+        N=10
+        
+        # For class & idx, generate label/target data w/ image loads        
+        for key, value in self.classes.items():
+            
+            print(f'loading... category : {key}')
+            path=os.path.join(self.datadir,key)
+            i = 0
+            
+            for img in os.listdir(path):
+                if i >= N:
+                    break
+                img_array=imread(os.path.join(path,img))
+                img_resized=resize(img_array,(150,150,3))
+                flat_data_arr.append(img_resized.flatten())
+                target_arr.append(value)
+                i+=1
+                
+        flat_data=np.array(flat_data_arr)
+        target=np.array(target_arr)
+        
+        # Send to DataFrame, Extract x & y vals
+        df=pd.DataFrame(flat_data) 
+        df['Target']=target
+        x=df.iloc[:,:-1] 
+        y=df.iloc[:,-1]
+        
+        # Split Data
+        self.x_train,self.x_test,self.y_train,self.y_test=train_test_split(x,y,test_size=0.20,
+                                               random_state=0,
+                                               stratify=y)
+    def train(self):
+        '''
+        Train SVM Model
+        GridSearchCV for optimal parameters
+        Args:
+            None
+        Returns:
+            None
+        '''
+        # Setup Param Dict for GridSearchCV
+        param_grid={'C':[0.1,1,10,100],
+            'gamma':[0.0001,0.001,0.1,1],
+            'kernel':['rbf','poly']}
+  
+        svc=svm.SVC(probability=True)
+        self.model=GridSearchCV(svc,param_grid)
+        
+        # Fit model
+        self.model.fit(self.x_train,self.y_train)
+        
+    def test(self):
+        ''''
+        Get Predictions For Test Data
+        Calculate accuracy
+        Args:
+            None
+        Returns:
+            None
+        '''
+        y_pred = self.model.predict(self.x_test)
+        accuracy = self.accuracy_score(y_pred, self.y_test)
+        print(f"The model is {accuracy*100}% accurate")
+        
+    def export(self):
+        '''
+        Dump Trained Model to .pkl File
+        Args:
+            None
+        Returns:
+            None
+        '''
+        joblib.dump(self.model.best_estimator_, os.getcwd() + '/models/svm_model.pkl', compress = 1)
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from scripts.make_dataset import *
 from scripts.build_features import *
+from scripts.make_dataset import *
 from scripts.model import *
+from scripts.svm_model import *
 
 if __name__ == '__main__':
     MakeDataset()
@@ -12,12 +13,22 @@ if __name__ == '__main__':
         test_path = './data/processed/asl_alphabet_test'
     )
     
+    # Transfer Learning Model - Data, DataLoaders
     bf.build_transformers()
     train_data, test_data = bf.build_data()
     trainloader, testloader = bf.build_dataloaders()
     
+    # Transfer Learning - Setup, Train, Test, Export
     ms = ModelSetup(train_data, trainloader, test_data, testloader, model = '')#'/models/mobilenetv2_asl_letnum.pkl')
     ms.setup()
     ms.train()
     test_acc, recall_vals = ms.test()
     ms.export()
+    
+    # SVM Model - Setup, Train, Test, Export
+    svm = SVMModel()
+    svm.load()
+    svm.train()
+    svm.test()
+    svm.export()
+    
